74f17470ffb71879c6f85c16b260e61906cdf1ad
Ever Joel Ccencho Santacruz <110436144+EvryDash05@users.noreply.github.com>
2025-11-22 01:44:36 -0500

    Update  cors

--- src/main/java/com/api/auth_microservice/infrastructure/security/constants/UrlConstants.java
+++ src/main/java/com/api/auth_microservice/infrastructure/security/constants/UrlConstants.java
@@ -19,7 +19,8 @@ public class UrlConstants {
 
     public static final String[] DEV_URLS = {
             "http://localhost:5173",
-            "http://localhost:3000"
+            "http://localhost:3000",
+            "https://fronbilletera-digital-production.up.railway.app/"
     };
 
     private UrlConstants() {

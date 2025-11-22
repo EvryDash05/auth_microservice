50acfee4014e6a6be07b6687d2070868e51c0cd9
Ever Joel Ccencho Santacruz <110436144+EvryDash05@users.noreply.github.com>
2025-11-22 01:06:23 -0500

    Update cors

--- src/main/java/com/api/auth_microservice/infrastructure/security/constants/UrlConstants.java
+++ src/main/java/com/api/auth_microservice/infrastructure/security/constants/UrlConstants.java
@@ -18,7 +18,8 @@ public class UrlConstants {
     };
 
     public static final String[] DEV_URLS = {
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "http://localhost:3000"
     };
 
     private UrlConstants() {
